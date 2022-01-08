12b46efbd7a5a7bcc8cd72cc7e128df38450b167
Ranjith kumar <ranjit4072@gmail.com>
2022-01-08 17:12:20 +0530

    Added search/query params

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,21 +1,47 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useSearchParams } from 'react-router-dom'
 
 const Users = ({users}) => {
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    let searchTerm = searchParams.get('name') || ''
+
+    let handleSearch = (event) => {
+        let name = event.target.value
+        if (name) {
+            setSearchParams({ name: event.target.value, length: event.target.value.length })
+        } else {
+            setSearchParams({})
+        }
+    }
+
     return (
         <div style = {{display: 'flex', flexDirection: 'column'}}>
             <h2>Users</h2>
+
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearch}
+            />
+
             <ul>
                 {
-                    users.map((user) => {
-                        return (
-                            <li>
-                                <Link to = {`${user.id}`}>
-                                    {user.fullName}
-                                </Link>
-                            </li>
+                    users
+                        .filter((user) =>
+                            user.fullName.toLowerCase()
+                            .includes(searchTerm.toLocaleLowerCase())
                         )
-                    })
+                        .map((user) => {
+                            return (
+                                <li>
+                                    <Link to = {`${user.id}`}>
+                                        {user.fullName}
+                                    </Link>
+                                </li>
+                            )
+                        })
                 }
             </ul>
             <Outlet />
